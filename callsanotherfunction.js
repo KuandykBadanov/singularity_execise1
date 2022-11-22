19d7fa119f12d8289ee2719a652a5ae1b64338cb
KuandykBadanov <118505559+KuandykBadanov@users.noreply.github.com>
2022-11-22 18:52:07 +0600

    Add files via upload
    calls another function some fixed amount of times

---
+++ callsanotherfunction.js
@@ -0,0 +1,13 @@
+function Gandalf() {
+    console.log("You shall not pass!")
+ }
+ 
+ call(3, Gandalf)
+ // You shall not pass!
+ // You shall not pass!
+ // You shall not pass!
+ 
+ call(1, function run() {
+    console.log("Fly, you fools!")
+ })
+ // Fly, you fools!
