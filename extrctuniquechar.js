6a57dbc792f6a093d7fd95a1de60d9f0705f5ddb
KuandykBadanov <118505559+KuandykBadanov@users.noreply.github.com>
2022-12-08 08:44:07 +0600

    Update extrctuniquechar.js

--- extrctuniquechar.js
+++ extrctuniquechar.js
@@ -1,16 +1,6 @@
-function unique_char(str1)
-{
- var str=str1;
- var uniql="";
- for (var x=0;x < str.length;x++)
- {
+function uniqueChar(str) {
+  const set = new Set(str.split(""));
+  return [...set].join("");
+}
 
- if(uniql.indexOf(str.charAt(x))==-1)
-  {
-  uniql += str[x];  
-  
-   }
-  }
-  return uniql;  
-}  
-console.log(unique_char("thequickbrownfoxjumpsoverthelazydog"));
+console.log(uniqueChar("thequickbrownfoxjumpsoverthelazydog")); // thequickbrownfxjmpsvlazydg
