4032704e91cf9e0d6c907dee9e6a349fdd4434c1
KuandykBadanov <118505559+KuandykBadanov@users.noreply.github.com>
2022-12-08 08:34:10 +0600

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,4 +1,10 @@
-isPalindrome("loki"); // false
-isPalindrome("aba"); // true
-isPalindrome("abab"); // false
-isPalindrome("Qazaq"); // true
+function isPalindrome(str) {
+  const lower = str.toLowerCase();
+  const reversed = lower.split("").reverse().join("");
+  return lower === reversed;
+}
+
+console.log(isPalindrome("loki")); // false
+console.log(isPalindrome("aba")); // true
+console.log(isPalindrome("abab")); // false
+console.log(isPalindrome("Qazaq")); // true
