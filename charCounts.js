182ba2a13008999f3ef5cdb467b9680a790d7720
KuandykBadanov <118505559+KuandykBadanov@users.noreply.github.com>
2022-12-08 08:51:59 +0600

    Update and rename getthenumber.js to charCounts.js

---
+++ charCounts.js
@@ -0,0 +1,21 @@
+function charCounts(str) {
+  const obj = {};
+
+  const arr = str.split("");
+
+  for (const a of arr) {
+    if (a === " ") {
+      continue;
+    }
+
+    if (a in obj) {
+      obj[a]++;
+    } else {
+      obj[a] = 1;
+    }
+  }
+
+  return obj;
+}
+
+console.log(charCounts("The quick brown fox jumps over the lazy dog")); // thequickbrownfxjmpsvlazydg
