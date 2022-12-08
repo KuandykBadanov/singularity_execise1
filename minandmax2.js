9fb95db1f98c17ae85d7dd855cb417afc7cdede2
KuandykBadanov <118505559+KuandykBadanov@users.noreply.github.com>
2022-12-08 08:40:53 +0600

    Update minandmax2.js

--- minandmax2.js
+++ minandmax2.js
@@ -1,21 +1,8 @@
-function Second_Greatest_Lowest(arr_num)
-{
-  arr_num.sort(function(x,y)
-           {
-           return x-y;
-           });
-  var uniqa = [arr_num[0]];
-  var result = [];
-  
-  for(var j=1; j < arr_num.length; j++)
-  {
-    if(arr_num[j-1] !== arr_num[j])
-    {
-      uniqa.push(arr_num[j]);
-    }
-  }
-    result.push(uniqa[1],uniqa[uniqa.length-2]);
-  return result.join(',');
-  }
+function minAndMax2(arr) {
+  const sorted = arr.sort((a, b) => a - b);
 
-console.log(Second_Greatest_Lowest([1,2,3,4,5]));
+  return [sorted[1], sorted[sorted.length - 2]];
+}
+
+console.log(minAndMax2([1, 7, 5, 10, 2, 20, 12, 11, 16, 14, 9])); // [5, 17]
+console.log(minAndMax2([6, 7, 16, 11, 12, 10, 9, 4, 1, 15])); // [4, 15]
