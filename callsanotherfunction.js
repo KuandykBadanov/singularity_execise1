fdc4922371f08b3291eafc88583874a269275432
KuandykBadanov <118505559+KuandykBadanov@users.noreply.github.com>
2022-12-08 10:14:36 +0600

    Update callsanotherfunction.js

--- callsanotherfunction.js
+++ callsanotherfunction.js
@@ -1,13 +1,19 @@
 function Gandalf() {
-    console.log("You shall not pass!")
- }
- 
- call(3, Gandalf)
- // You shall not pass!
- // You shall not pass!
- // You shall not pass!
- 
- call(1, function run() {
-    console.log("Fly, you fools!")
- })
- // Fly, you fools!
+   console.log("You shall not pass!")
+}
+
+function call(num, func) {
+  for (i = 0; i < num; i++) {
+    func();
+  }
+  
+} 
+call(3, Gandalf)
+// You shall not pass!
+// You shall not pass!
+// You shall not pass!
+
+call(1, function run() {
+   console.log("Fly, you fools!")
+})
+// Fly, you fools!
